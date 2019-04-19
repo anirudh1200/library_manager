b39a257096c0af6deb49a5c069e633a7711ab278
= <anirudh.b.123@gmail.com>
2019-04-19 17:07:55 +0530

    book selection enabled

--- src/components/member/MemberDetails.js
+++ src/components/member/MemberDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, FlatList } from 'react-native';
+import { SearchBar, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class MemberDetails extends Component {
 
@@ -12,7 +13,10 @@ class MemberDetails extends Component {
 
 	state = {
 		...this.props.navigation.getParam('member', 'notfound'),
-		search: ''
+		search: '',
+		modalVisible: false,
+		allBooks: this.props.allBooks,
+		selectedBook: ''
 	};
 
 	updateSearch = search => {
@@ -20,25 +24,131 @@ class MemberDetails extends Component {
 	};
 
 	render() {
-		console.log(this.state);
+		const { allBooks, search } = this.state;
+		console.log(allBooks);
+		const books = allBooks.filter(e => (e.name.indexOf(search) !== -1));
+		console.log("Books");
+		console.log(books);
+		console.log(search);
 		return (
-			<View>
-				<Text>Mobile Number: {this.state.number}</Text>
-				<SearchBar
-					placeholder="Search Book..."
-					onChangeText={this.updateSearch}
-					value={this.state.search}
-				/>
-				<TouchableOpacity
-					onPress={() => {
-						console.log('selected');
-					}}
-				>
-					<Text>Add Book</Text>
-				</TouchableOpacity>
+			<View style={styles.body}>
+				<Modal
+					animationType="slide"
+					transparent={false}
+					visible={this.state.modalVisible}
+					onRequestClose={() => {
+						this.setState({ modalVisible: false })
+					}}>
+					<View style={styles.body}>
+						<View style={styles.top}>
+							<SearchBar
+								lightTheme
+								placeholder="Search Book..."
+								onChangeText={this.updateSearch}
+								value={this.state.search}
+								containerStyle={styles.container}
+								inputContainerStyle={styles.inputContainerStyle}
+								autoCorrect={false}
+							/>
+						</View>
+						<View style={styles.middle}>
+							<FlatList
+								data={books}
+								keyExtractor={(item, index) => index.toString()}
+								renderItem={({ item }) => (
+									<TouchableOpacity
+										onPress={() => this.setState({ selectedBook: item.name })}
+									>
+										<ListItem
+											title={item.name}
+											subtitle={item.language}
+										/>
+									</TouchableOpacity>
+								)}
+							/>
+						</View>
+					</View>
+				</Modal>
+				<View style={styles.body}>
+					<View style={styles.top}>
+						<Text style={styles.header}>Mobile Number: {this.state.number}</Text>
+					</View>
+					<View style={styles.middle}>
+						<Text>Anirudh</Text>
+					</View>
+					<View style={styles.bottom}>
+						<TouchableOpacity
+							style={styles.button}
+							onPress={() => {
+								this.setState({ modalVisible: true });
+							}}
+						>
+							<Text style={styles.btnText}>Add Book</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
 			</View>
 		)
 	}
 }
 
-export default MemberDetails;
+const mapStateToProps = state => {
+	return {
+		allBooks: state.books.books
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemberDetails);
+
+const styles = StyleSheet.create({
+	body: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		padding: 15,
+		alignItems: 'stretch',
+	},
+	top: {
+		flex: 2
+	},
+	header: {
+		fontSize: 18,
+		color: '#000',
+		paddingBottom: 10,
+		marginBottom: 10,
+		borderBottomColor: '#199187',
+		borderBottomWidth: 1,
+	},
+	container: {
+		margin: 1,
+		padding: 1,
+		backgroundColor: 'white',
+		borderWidth: 0,
+		height: 50
+	},
+	button: {
+		alignSelf: 'stretch',
+		alignItems: 'center',
+		padding: 15,
+		backgroundColor: '#03a9f4',
+	},
+	btnText: {
+		color: '#fff',
+		fontWeight: 'bold',
+	},
+	inputContainerStyle: {
+		backgroundColor: 'white'
+	},
+	middle: {
+		flex: 10
+	},
+	bottom: {
+		flex: 1,
+	}
+});
